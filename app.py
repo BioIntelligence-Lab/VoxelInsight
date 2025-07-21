3d813d69502bd1af186ca088252ddfa68274dbba
Parekh <vishwaparekh@gmail.com>
2025-07-21 13:07:04 -0500

    Updated with additional features

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ from idc_index import index
 
 IDC_Client = index.IDCClient()
 df_IDC = IDC_Client.index
-df_MIDRC = pd.read_csv("C:/Users/vpare/OneDrive - UTHealth Houston/Research/ARPA_H/midrc_distributed_subjects.csv")
+df_BIH = pd.read_csv("Data/midrc_distributed_subjects.csv")
+df_MIDRC = pd.read_csv("Data/MIDRC_Cases_table.csv")
 def parse(chat_response):
   #print('H1')
   #code_blocks = re.findall(r"```sql(.+?)```", chat_response, re.DOTALL)
@@ -43,7 +44,15 @@ def parse(chat_response):
 async def on_message(message: cl.Message):
     
     pretext= 'Please be as specific as possible and only return the final python code enclosed in ```. \
-    Do not provide explanations. I have created two dataframes \
+    Do not provide explanations. You are a medical imaging research assistant.\
+    Your name is VoxelInsight. You are part of BioInsight\
+    You have the following key capabilities right now.\
+    (1) User can interact with public and private datasets with natural language and ask questions like how many collections are present in X\
+    (2) User can do standard image processing like nifti conversion, image registration, etc.\
+    (3) User can segment medical imaging datasets\
+    (4) User can extract imaging biomarkers using radiomics\
+    The details about all the capabilities are as follows\
+    I have created two dataframes \
     the first dataframe contains all the data for the platform IDC and is created using the package idc-index. \
     This data frame was created using the following command: IDC_Client = index.IDCClient(); df_IDC = IDC_Client.index. Assume df_IDC is already present. \
     Using the pandas dataframe, df_IDC which has data fields such as: \
@@ -62,10 +71,10 @@ async def on_message(message: cl.Message):
     Use your best guess if the user asks for information present outside the provided data fields. \
     Make sure the queries are case insensitive and use Regex wherever necessary. \
     If the user wants to download data, use the following commands: from idc_index import index; IDC_Client = index.IDCClient(); IDC_Client.download_from_selection(seriesInstanceUID=selected_series, downloadDir=".") \
-    The second dataframe df_MIDRC contains the multi-source indexed dataframe\
+    The second dataframe df_BIH contains the multi-source indexed dataframe\
     This index contains study level data from multiple source or public platforms including IDC, MIDRC, TCIA, among others\
     You have to identify whether the user wants to query this BDF dataframe or the IDC dataframe and answer their query\
-    The MIDC dataframe has the following fields\
+    The df_BIH dataframe has the following fields\
     subject_id: patient id \
     commons_name: name of the data source like IDC, MIDRC, AIMI\
     metadata_source_version: version of the metadata \
@@ -87,11 +96,15 @@ async def on_message(message: cl.Message):
     You also have access to the following tools if you are working with local data where the user provides path to the data: \
     1) DICOM to NIfTI conversion using the dicom2nifti Python package. \
     2) Image visualization using ipywidgets and matplotlib for viewing DICOM and NIfTI images. \
-    3) Segmentation using TotalSegmentator, which supports organ and lesion segmentation from CT/MRI NIfTI files. \
+    3) Segmentation: Supports two algorithmic implementations: TotalSegmentator and NNUNet_v2 \
+    TotalSegmentator, which supports organ and lesion segmentation from CT/MRI NIfTI files. \
     Example Command line usage: TotalSegmentator -i ct.nii.gz -o segmentations -ta <task_name> -rs <roi_subset>\
     example for normal tissue: TotalSegmentator -i ct.nii.gz -o seg -ta total -rs liver\
     For tumor, the task is different: here is an example: TotalSegmentator -i ct.nii.gz -o seg -ta liver_vessels\
     Here seg is the folder name\
+    nnunet: currently supports brain tumor segmentation\
+    Here is an example command if the user asks to perform brain tumor segmentation. The task number is 501\
+    nnUNetv2_predict -i INPUT_DATA_PATH -o OUTPUT_DATA_PATH -d 501 -c 3d_fullres -f 0\
     4) Radiomics extraction using PyRadiomics, which computes shape, first-order, and texture features from segmented regions. \
     Usage Command line: pyradiomics <path/to/image> <path/to/segmentation> -o results.csv -f csv\
     When a user asks a clinical imaging question (e.g., “What is the liver volume in this scan?”), you should: \
@@ -125,7 +138,7 @@ async def on_message(message: cl.Message):
     
 
     # Create a local namespace
-    #local_vars = {"df_IDC": df_IDC,"df_MIDRC": df_MIDRC, "pydicom":pydicom,"os":os}
+    #local_vars = {"df_IDC": df_IDC,"df_BIH": df_BIH, "pydicom":pydicom,"os":os}
     # Add everything you might use to local_vars
     local_vars = {
     "os": os,
@@ -133,14 +146,20 @@ async def on_message(message: cl.Message):
     "plt": plt,
     "io": io,
     "df_IDC": df_IDC,
-    "df_MIDRC": df_MIDRC
+    "df_BIH": df_BIH
     }
+
+    
+    await cl.Message(content=f"I have generated the following python code in response to your query:\n```python\n{q}\n```").send()
+    await cl.Message(content="# Result: \n").send()
     exec(q, local_vars, local_vars)
     #exec(q, globals(), globals())
 
     # Now access the variable
     res_query = local_vars.get("res_query")
     print(res_query)
+
+    
 
     if res_query is None:
             await cl.Message(content="No result found in res_query.").send()
